2c08d7839aa5e9538a87f63273885dd2d0477456
znareak <libardojesusrengifo129@gmail.com>
2021-06-23 17:19:28 -0400

    dev: Update and create a post

--- api/components/post/controller.js
+++ api/components/post/controller.js
@@ -1,8 +1,24 @@
+const { nanoid } = require("nanoid");
 const TABLE = "posts";
-module.exports = (store = require("../../../store/dummy")) => {
+
+module.exports = (store = require("../../../store/mysql")) => {
   return {
     list() {
       return store.list(TABLE);
+    },
+
+    get(id) {
+      return store.get(TABLE, id);
+    },
+
+    upsert(payload) {
+      payload.id = nanoid();
+      return store.upsert(TABLE, payload, true);
+    },
+
+    update(id, payload) {
+      const postEdited = store.update(TABLE, payload, id);
+      return postEdited;
     },
   };
 };
--- api/components/post/network.js
+++ api/components/post/network.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const { success } = require("../../../network/response");
-const { list } = require("./index");
+const { list, get, upsert, update } = require("./index");
 
 router.get("/", async (req, res) => {
-  const users = await list();
-  success(req, res, users);
+  const posts = await list();
+  success(req, res, posts);
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await get(id);
+  success(req, res, post?.[0]);
+});
+
+router.post("/", async (req, res) => {
+  const { text, user } = req.body;
+  const postCreated = await upsert({ text, user });
+  success(req, res, postCreated);
+});
+
+router.put("/:id", async (req, res) => {
+  const postUpdated = await update(req.params.id, req.body);
+  success(req, res, postUpdated);
 });
 
 module.exports = router;
